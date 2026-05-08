e806b8c5eaee66446433b986c617f09c80693419
Faizan Ali <f.ali@spctek.com>
2026-05-08 23:16:13 +0500

    update date in swagger

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(
-    version="1.0.5.5",
+    version="1.0.5.8",
     title="SPCTEK AI API",
     description="Backend API for SPCTEK AI platform",
     lifespan=lifespan,
